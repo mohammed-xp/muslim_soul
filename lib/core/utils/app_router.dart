5f1569f9fb626bd42882f9d345b9e1b1e276b799
Mohammed Adil <66908713+mohammed-xp@users.noreply.github.com>
2024-04-23 01:39:20 +0200

    Add Surah Details view to AppRouter
    Include the Surah Details view in the AppRouter routes for navigation.

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -2,12 +2,14 @@ import 'package:go_router/go_router.dart';
 import 'package:muslim_soul/features/main/presentation/views/main_view.dart';
 import 'package:muslim_soul/features/onboarding/presentation/views/onboarding_view.dart';
 import 'package:muslim_soul/features/quran/presentation/views/juz_view.dart';
+import 'package:muslim_soul/features/quran/presentation/views/surah_details_view.dart';
 import 'package:muslim_soul/features/splash/presentation/views/splash_view.dart';
 
 abstract class AppRouter {
   static const kMainView = '/mainView';
   static const kOnboardingView = '/onboardingView';
   static const kJuzView = '/juzView';
+  static const kSurahDetailsView = '/surahDetailsView';
 
   static final router = GoRouter(
     routes: [
@@ -26,6 +28,10 @@ abstract class AppRouter {
       GoRoute(
         path: kJuzView,
         builder: (context, state) => const JuzView(),
+      ),
+      GoRoute(
+        path: kSurahDetailsView,
+        builder: (context, state) => const SurahDetailsView(),
       ),
     ],
   );
